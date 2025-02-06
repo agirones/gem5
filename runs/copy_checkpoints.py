2060ca74915f9ce8631c30789e2d480e4bfca588
Amund Bergland Kvalsvik <amund.kvalsvik@ntnu.no>
2025-02-06 16:53:40 +0100

    copy checkpoints script added

---
+++ runs/copy_checkpoints.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+dirs = os.listdir(".")
+
+print(dirs)
+for name in dirs:
+    if not name.startswith("6"):
+        continue
+    cpt_name = [entry for entry in os.listdir(f"{name}/m5out") if entry.startswith("cpt")]
+    assert(len(cpt_name) == 1)
+    cpt_name = cpt_name[0]
+    src = f"{name}/m5out/{cpt_name}"
+    tgt = f"checkpoints/{name}-cpt"
+    shutil.copytree(src, tgt)
+
