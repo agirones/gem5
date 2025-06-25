50676d4bb975be103c1936f173baeb626cf5a052
Andreu Girones <girones96@gmail.com>
2025-06-25 16:59:00 +0200

    resources,configs,python: Add local resource config and kernel
    This commit introduces a new local 'resources/' directory containing
    'local-resources.json' and an 'x86-linux-kernel-6.8.0-35-generic'
    kernel image.
    
    The 'src/python/my-gem5-config.py' file is added to serve as a
    custom resource configuration for gem5. It enables the gem5 resource
    system to discover and utilize these locally provided resources,
    including the specified kernel, when referenced in simulation scripts.

---
+++ src/python/my-gem5-config.py
@@ -0,0 +1,8 @@
+{
+    "sources": {
+        "local-resources": {
+            "url": "/cluster/home/andreug/research/gem5-resources/src/spec-2017/gem5/resources/local-resources.json",
+            "isMongo": false
+        }
+    }
+}
